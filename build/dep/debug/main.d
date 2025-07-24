8f0b4f9247ca5f3296dd89fe1ecb0e6b1bd19458
Eddy <battaglia.eddy@gmail.com>
2025-07-24 22:24:21 +0200

    correction

--- build/dep/debug/main.d
+++
@@ -1,3 +0,0 @@
-./build/obj/debug/main.o build/dep/debug/main.d : src/main.c inc/example.h
-	mkdir -p ./build/obj/debug
-	gcc -o ./build/obj/debug/main.o -c src/main.c -W -Wall -ansi -pedantic -g -I ./inc
